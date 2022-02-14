b235827f7c54432b1f258d19236fafa3ad35eebc
Shivam Sinha <shivam.sinha@zemosolabs.com>
2022-02-14 09:37:12 +0530

    pushing javascript

---
+++ assignment/Javascript/as2.js
@@ -0,0 +1,2 @@
+let func=(firstName,lastName) => firstName[0]+lastName[0];
+console.log(func("Shivam","Sinha"));
---
+++ assignment/Javascript/assignment1.js
@@ -0,0 +1,11 @@
+var add = function(a, b) {
+    return a+b;
+}
+
+var op = function (func) {
+    var x = 2;
+    var y = 3;
+    return func(x, y);
+}
+
+console.log(op(add));
