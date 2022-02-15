2b1f2711efe1fe19fb3cc63b570ce1edc071d1e4
Shivam Sinha <shivam.sinha@zemosolabs.com>
2022-02-15 09:48:28 +0530

    pushing javascript assignment 3 of ES6

---
+++ assignment/Javascript/assignmentES6.js
@@ -0,0 +1,23 @@
+const printName=(name) => "Hi" + name;
+console.log(printName("Shivam"));
+
+
+
+
+const printBill = (name, bill) => `Hi ${name} ,please pay bill: ${bill}`;
+
+console.log(printBill(' Shivam',100));
+
+
+
+const person= {
+    name:"Noam Chomsky",
+    age:92
+}
+
+const {name,age}=person;
+console.log(name);
+console.log(age);
+
+
+
